903bd9118fea94af3243b35ca61b1371d6c0f1ca
Ege_Perim <perim.ege@gmail.com>
2022-01-21 19:42:40 +0300

    database

--- connectiondb.php
+++ connectiondb.php
@@ -1,6 +1,14 @@
 <?php
 /*Author: Ege PERIM*/
-error_reporting(0);
-$connection_db=mysqli_connect("localhost","root","","tour_db");
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
 ?>
